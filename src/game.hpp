288fe51d43ce8b9ac52bfd14cd42e62ead333b9c
geodude1992 <oblivichiiipower@gmail.com>
2025-04-16 00:37:34 -0700

    Declarations in Game Class

--- src/game.hpp
+++ src/game.hpp
@@ -1,10 +1,14 @@
 #pragma once    // ensures that the contents are included only once, even if the header file is included multiple times in different parts of your project.
+#include "spaceship.hpp"
 
 class Game {
     public:
         Game();
         ~Game();
-        
+        void Draw();
+        void Update();
+        void HandleInput();
 
     private:
+        Spaceship spaceship;
 };
